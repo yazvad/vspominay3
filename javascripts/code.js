848063ae7a3f7dfaee64b46ae944c2052cedc346
yazvad <146091000+yazvad@users.noreply.github.com>
2024-03-29 04:10:22 +0300

    Add files via upload

---
+++ javascripts/code.js
@@ -0,0 +1,4 @@
+$(document).ready(function()) {
+    $('.star1, .star2, star3, star4, .star5, .star6').draggable();
+
+}
---
+++ javascripts/jquery-3.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $(".starwars img").draggable();
+  });
